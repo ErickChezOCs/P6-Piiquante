d1cd6b5b1f80f9e018db8762c181abd5d6510ac4
LihaaDjem <lihaylipubi@gmail.com>
2023-01-30 15:43:17 +0100

    modification de likedSauce

--- back_end/controllers/sauces.js
+++ back_end/controllers/sauces.js
@@ -69,8 +69,6 @@ exports.modifySauce = (req, res, next) => {
     });
 };
 
-  
-
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
@@ -109,76 +107,53 @@ exports.getAllSauce = (req, res, next) => {
   
 
 exports.likedSauce = (req, res, next) => {
-  // si userId se trouve déja dans usersLiked ou usersDisliked
- // il faut d'abord modifier ce statut avant qu'il puisse à nouveau 
- // liker ou disliker c'est la première condition à vérifier
- let etatLike = Sauce.usersLiked.includes(req.body.userId);
- let etatDislike =  Sauce.usersDisliked.includes(req.body.userId);
- console.log(etatLike,etatDislike);
+  // vérifier l'état courant de like compris entre 1 et -1
+  if(req.body.like <= 1 && req.body.like >= -1) {
+      Sauce.findOne({_id: req.params.id})
+          .then(sauce => {
+            // supprimer s'il y en a les doublons de userId dans usersLiked et Disliked
+              sauce.usersLiked = [...new Set(sauce.usersLiked)]; 
+              sauce.usersDisliked = [...new Set(sauce.usersDisliked)];
+              switch (req.body.like) {
+                  // parcourir les trois cas possibles
+                  case 0 : 
+                      if (sauce.usersLiked.includes(req.body.userId)) {
+                          sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.body.userId), 1);
+                          sauce.likes = sauce.usersLiked.length;
+                      } else if (sauce.usersDisliked.includes(req.body.userId)){
+                          sauce.usersDisliked.splice(sauce.usersLiked.indexOf(req.body.userId), 1);
+                          sauce.dislikes = sauce.usersDisliked.length;
+                      } else {
+                          throw err;
+                      }
+                      break;
+                  case 1 : 
+                      if (!sauce.usersLiked.includes(req.body.userId)){
+                          sauce.usersLiked.push(req.body.userId);
+                          sauce.likes = sauce.usersLiked.length;
+                          break;
+                      } else {
+                          throw err;
+                      }
+                  case -1 :
+                      if (!sauce.usersDisliked.includes(req.body.userId)) {
+                          sauce.usersDisliked.push(req.body.userId);
+                          sauce.dislikes = sauce.usersDisliked.length;
+                          break;
+                      } else {
+                          throw err;
+                      }
+              }
+              Sauce.updateOne({_id: req.params.id}, sauce)
+                  .then(() => res.status(200).json("liked or not!"))
+                  .catch(error => res.status(400).json({error}));
+              })
+          .catch(error => res.status(404).json({error}));         
+  } else {
+      throw err;
+  }
 
- // Le code  doit s'exécuter que pour un etatLike ou etatDislike false
- // pour un etatLike false like peut avoir la valeur 1
- if (!etatLike)  {
-  if (req.body.like === 1) {
-    Sauce.updateOne(
-      { _id: req.params.id },
-      { $inc: { likes: +1 }, $push: { usersLiked: req.body.userId } }
-    )
-      .then(() => {
-        res.status(201).json({ message: "One like" });
-        console.log("l'utilisateur a liké!");
-      })
-      .catch((error) => res.status(400).json(error));
-  }
-  // sinon la valeur autorisée est 0
- } else {
-  if (req.body.like === 0) {
-    Sauce.findOne({ _id: req.params.id }).then((Sauce) => {
-      if (Sauce.usersLiked.includes(req.body.userId)) {
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId } }
-        )
-          .then(() => {
-            res.status(201).json({ message: "No more like" });
-          })
-          .catch((error) => res.status(400).json(error));
-      }
-    });
-  }  
- }
-// la même logique doit s'appliquer à dislike, l'utilisateur ne peut disliker que s'il
-// ne l'a déja fait sinon il doit d'abord supprimer son précédent dislike
-if(!etatDislike) {
-  if ( req.body.like === -1) {
-    Sauce.updateOne(
-      { _id: req.params.id },
-      { $inc: { dislikes: +1 }, $push: { usersDisliked: req.body.userId } }
-    )
-      .then(() => {
-        res.status(201).json({ message: "One dislike" });
-        console.log("l'utilisateur a disliké!");
-      })
-      .catch((error) => res.status(400).json(error));
-  }  
-} else {
-  if (req.body.like === 0) {
-    Sauce.findOne({ _id: req.params.id }).then((Sauce) => {
-      if (Sauce.usersDisliked.includes(req.body.userId)) {
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId } }
-        )
-          .then(() => {
-            res.status(201).json({ message: "No more dislike" });
-          })
-          .catch((error) => res.status(400).json(error));
-      }
-    });
-  }
-};
 }
-  
 
 
 
